61bd758ed53299a71b97320c15159f23a9ff5f25
jmouchawar <31359407+jmouchawar@users.noreply.github.com>
2017-10-26 16:20:46 -0700

    Add files via upload

--- plot_tree.py
+++ plot_tree.py
@@ -1,10 +1,14 @@
 import graphviz
+import pydotplus
 from sklearn.tree import export_graphviz
 
 def plot_decision_tree(dtc, feature_names, class_names):
     
-    export_graphviz(dtc, out_file="temp.dot", feature_names=feature_names, class_names=class_names, filled = True, impurity = False)
-    with open("temp.dot") as f:
+    export_graphviz(dtc, out_file="tmp.dot", 
+                    feature_names=feature_names, class_names=class_names, 
+                    filled = True, impurity = False)
+    
+    with open("tmp.dot") as f:
         dot_graph = f.read()
-
+        
     return graphviz.Source(dot_graph)
